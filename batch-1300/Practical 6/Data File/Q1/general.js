8dea5adc011674dd2058d9bbc57724c6446640a7
kahshiu <kahshiu@gmail.com>
2022-03-19 15:05:53 +0800

    select control

--- batch-1300/Practical 6/Data File/Q1/general.js
+++ batch-1300/Practical 6/Data File/Q1/general.js
@@ -1,1 +1,23 @@
-﻿
+﻿function display(el, rowNo) {
+    var value = el.value;
+    var index = el.selectedIndex;
+    var valueNum = Number(value);
+
+    var priceElem = document.getElementById("Price"+rowNo);
+    var quantityElem = document.getElementById("Quantity"+rowNo);
+    var amountElem = document.getElementById("Amount"+rowNo);
+    
+    var qValue = quantityElem.value;
+    var qValueNum = Number(qValue);
+
+    if(index > 0){
+        priceElem.value = value;
+        quantityElem.disabled = false;
+        amountElem.value = valueNum*qValueNum;
+    } else {
+        priceElem.value = "value";
+        quantityElem.disabled = true;
+        amountElem.value = 0;
+    }
+    
+}
