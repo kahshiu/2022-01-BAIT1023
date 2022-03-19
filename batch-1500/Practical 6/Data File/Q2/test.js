5e6a2957750ca6c05e773e11f79f71ee4796e223
kahshiu <kahshiu@gmail.com>
2022-03-19 16:15:43 +0800

    mistake

--- batch-1500/Practical 6/Data File/Q2/test.js
+++ batch-1500/Practical 6/Data File/Q2/test.js
@@ -1,17 +1,18 @@
+
 /*
-var hello = 1;
-var world = " ba ba black sheep ";
-var combined = hello.toString() + world;
+var hello = 1123123;
+var world = 222;
+var str = "something here"
+
+var combined = world.toString() + str + hello.toString();
+
 
 combined
-hello
-world
+var x = hello + world
+x
 */
 
 function dtFormat(dt) {
-    var hhNum = dt.getHours();
-    var miNum = dt.getMinutes();
-    var ssNum = dt.getSeconds();
     var mmNum = dt.getMonth();
     var ddNum = dt.getDate();
     var yyyyNum = dt.getFullYear();
@@ -19,52 +20,44 @@ function dtFormat(dt) {
     Jan -- 0
     Feb -- 1
     Mac -- 2
-    ...
-    Dec -- 11
     */
     var mmShort = [
-        'Jan', // 0
-        'Feb', // index 1
-        'Mac', // index 2
-        'Apr', // index 3...
+        'Jan', // index: 0
+        'Feb', // index: 1
+        'Mac', // 2
+        'Apr', // 3...
     ]
-    
+    var format1 = ddNum.toString().padStart(2, "0")
+        + "/" + (mmNum + 1).toString().padStart(2, "0")
+        + "/" + yyyyNum.toString();
 
-    var format1 = ddNum.toString() 
-      + "/" + mmNum.toString() 
-      + "/" + yyyyNum.toString()
-    
-    //MAN KEI: hh:mm:ss, 5min
-    var format2 = mmShort[mmNum] +
-        " " + ddNum.toString() +
-        ", " + yyyyNum.toString() +
-        ", " + hhNum.toString() +
-        ": " + miNum.toString() +
-        ": " + ssNum.toString() 
-    return [format1, format2];
+    var format2 = mmShort[mmNum]
+        + " " + ddNum.toString()
+        + ", " + yyyyNum.toString();
+
+    return [format1, format2]
 }
 
-function diffDays(dt,dt2){
-    var diff = dt2.getTime() - dt.getTime();
-    var ms1Day = 24*60*60*1000
-    var countDay = Math.round(diff/ms1Day);
-    return countDay;
+function diffDays (dt1,dt2) {
+    var msDiff = dt2.getTime() - dt1.getTime();
+    var ms1Day = 24*60*60*1000;
+    var dayDiff = Math.round(msDiff/ms1Day);
+    return dayDiff;
 }
 
 function printText() {
     var dt = new Date();
-    var nextYear = dt.getFullYear() +1;
-    var formatted = dtFormat(dt)[1];
+    var formatted = dtFormat(dt);
+    var nextYear = dt.getFullYear() + 1;
     var dt2 = new Date(nextYear, 0, 1);
-
-    var diff = diffDays(dt,dt2)
-    text1 = "Count down to new year: " + nextYear;
-    text2 = "Today is "+ formatted;
-    text3 = "Only " + diff + " days left before New Year " + nextYear;
-    return [text1,text2,text3];
+    var pickedFormat = formatted[1];
+    
+    var diff = diffDays(dt,dt2);
+    var text0 = "Count down to new year: " + nextYear;
+    var text1 = "Today is " + pickedFormat;
+    var text2 = "Only " + diff + " days left until new year";
+    return [text0, text1,text2]
 }
 
-
-
-// CTRL + SHIFT + P
-// quokka: start on current file
+var p = printText();
+p
