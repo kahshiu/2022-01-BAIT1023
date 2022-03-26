b04bab6d4fa3e3ca6c2fe89af21e2ca46763ab93
kahshiu <kahshiu@gmail.com>
2022-03-26 15:41:15 +0800

    first part

--- batch-1500/Practical 6/Data File/Q1/general.js
+++ batch-1500/Practical 6/Data File/Q1/general.js
@@ -1,1 +1,36 @@
-﻿
+
+// NOTE: remember html element's value is always in string form
+function calcAmount(priceString, quantityString){
+  var priceNo = Number(priceString);
+  var quantNo = Number(quantityString);
+  return quantNo * priceNo;
+}
+
+
+// NOTE: el is the one element that triggered "display"
+// rowno is row no of table 1,2,3
+// **press F12 to launch developer tools in chrome
+function display(el, rowno){
+  var selectedIndex = el.selectedIndex;
+  var selectedValue = el.value;
+  var rowString = rowno.toString();
+  
+  var elPrice = document.getElementById("Price" + rowString);
+  var elQuantity = document.getElementById("Quantity" + rowString);
+  var elAmount = document.getElementById("Amount" + rowString);
+
+  // 0: select an item
+  // 1: html + css
+  // 2: javascript ...
+  if(selectedIndex > 0) {
+    elPrice.value = selectedValue;
+    elQuantity.disabled = false;
+    var total = calcAmount(elPrice.value, elQuantity.value);
+    elAmount.value = total.toFixed(2); // NOTE: convert to 2 decimal value
+    
+  } else {
+    elPrice.value = "";
+    elQuantity.disabled = true;
+    elAmount.value = "0.00"
+  }
+}
