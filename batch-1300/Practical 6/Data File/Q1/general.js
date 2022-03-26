39432830426675253db038ba8f304865fba729aa
kahshiu <kahshiu@gmail.com>
2022-03-26 13:50:15 +0800

    shiu code with comments

--- batch-1300/Practical 6/Data File/Q1/general.js
+++ batch-1300/Practical 6/Data File/Q1/general.js
@@ -1,23 +1,61 @@
-﻿function display(el, rowNo) {
-    var value = el.value;
-    var index = el.selectedIndex;
-    var valueNum = Number(value);
+﻿function calcAmount(priceString, quantityString){
+    var priceNo = Number(priceString); // cast value (a string) to number
+    var quantityNo = Number(quantityString);
+    return quantityNo * priceNo; // return result to caller, in number format
+}
 
-    var priceElem = document.getElementById("Price"+rowNo);
-    var quantityElem = document.getElementById("Quantity"+rowNo);
-    var amountElem = document.getElementById("Amount"+rowNo);
+// NOTE: el is current html element that trigger this function
+// NOTE: rowno: row number: 1,2,3
+function display(el, rowNo) {
+    var selectedValue = el.value; // option's value 
+    var selectedIndex = el.selectedIndex; 
+    // selected index: 
+    // 0: Select an item,
+    // 1:HTML and CSS,
+    // 2,3...
+    var rowString = rowNo.toString();
+
+    var priceElem = document.getElementById("Price"+rowString);
+    var quantityElem = document.getElementById("Quantity"+rowString);
+    var amountElem = document.getElementById("Amount"+rowString);
     
-    var qValue = quantityElem.value;
-    var qValueNum = Number(qValue);
-
-    if(index > 0){
-        priceElem.value = value;
+    if(selectedIndex > 0){
+        priceElem.value = selectedValue;
         quantityElem.disabled = false;
-        amountElem.value = valueNum*qValueNum;
+
+        var amt = calcAmount(priceElem.value,quantityElem.value);
+        amountElem.value = amt.toFixed(2); // note: To add decimal point 2
     } else {
-        priceElem.value = "value";
+        priceElem.value = "";
         quantityElem.disabled = true;
-        amountElem.value = 0;
+        amountElem.value = "0.00";
+    }
+    
+}
+
+function sub(el, rowNo) {
+    var selectedValue = el.value; // option's value 
+    var selectedIndex = el.selectedIndex; 
+    // selected index: 
+    // 0: Select an item,
+    // 1:HTML and CSS,
+    // 2,3...
+    var rowString = rowNo.toString();
+
+    var priceElem = document.getElementById("Price"+rowString);
+    var quantityElem = document.getElementById("Quantity"+rowString);
+    var amountElem = document.getElementById("Amount"+rowString);
+    
+    if(selectedIndex > 0){
+        //priceElem.value = selectedValue;
+        //quantityElem.disabled = false;
+
+        var amt = calcAmount(priceElem.value,quantityElem.value);
+        amountElem.value = amt.toFixed(2); // note: To add decimal point 2
+    } else {
+        //priceElem.value = "";
+        //quantityElem.disabled = true;
+        amountElem.value = "0.00";
     }
     
 }
