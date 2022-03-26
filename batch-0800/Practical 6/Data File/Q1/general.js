d4e8f2980df1f22e0c2e66598a21e03b25d336cb
kahshiu <kahshiu@gmail.com>
2022-03-26 09:36:47 +0800

    full code

--- batch-0800/Practical 6/Data File/Q1/general.js
+++ batch-0800/Practical 6/Data File/Q1/general.js
@@ -1,4 +1,46 @@
-﻿function calcAmount(quantity, price){
+﻿function dtFormat(dt) {
+    /*MM
+Jan - 0
+Feb - 1
+Mac - 2 ...
+*/
+    var mmNum = dt.getMonth();
+    var ddNum = dt.getDate();
+    var yyyyNum = dt.getFullYear();
+    var miNum = dt.getMinutes();
+    var hhNum = dt.getHours();
+    var ssNum = dt.getSeconds();
+    
+    var mmString = [
+        'Jan', // index 0
+        'Feb', // index 1
+        'Mar', // INDEX 2
+        'Apr',
+        'May',
+        'Jun',
+        'Jul',
+        'Aug',
+        'Sep',
+        'Oct',
+        'Nov',
+        'Dec',
+    ]
+    var mmString2 = mmString[mmNum];
+
+    // show hour + minute, WONG YAN ZHI 
+    // format: Mar 19, 2022: hh: mm
+    var dtString = mmString2 
+        + " " + ddNum.toString() 
+        + ", " + yyyyNum.toString() 
+        + ": " + hhNum.toString() 
+        + ": " + miNum.toString() 
+        + ": " + ssNum.toString();
+    var dtString2 = ddNum.toString() + "/" + (mmNum+1).toString() + "/" + yyyyNum.toString();
+
+    return [dtString, dtString2];
+}
+
+function calcAmount(quantity, price){
     var quantityNo = Number(quantity);
     var priceNo = Number(price);
     return quantityNo * priceNo;
@@ -6,7 +48,6 @@
 
 function totalAmount(rowTotal){
     var total = 0;
-    var elTot = document.getElementById("totalAmount")
 
     for(var i=1; i<=rowTotal; i++){
         var rowString = i.toString();
@@ -19,8 +60,7 @@ function totalAmount(rowTotal){
         );
         total = total + amt;
     }
-    
-    elTot.value = total.toFixed(2);
+    return total;
 }
 
 function display(el, rowno){
@@ -40,10 +80,14 @@ function display(el, rowno){
             selectedValue
         ).toFixed(2);
     } else {
+        elPrice.value = '';
         elQuantity.disabled = true;
         elAmount.value = "0";
     }
-    totalAmount(3);
+
+    var total = totalAmount(3);
+    var elTot = document.getElementById("totalAmount")
+    elTot.value = total.toFixed(2);
 }
 
 //  NOTE: for quantity select dropdown
@@ -67,5 +111,50 @@ function sub(el, rowno){
         //elQuantity.disabled = true;
         elAmount.value = "0";
     }
-    totalAmount(3);
+    var total = totalAmount(3);
+    var elTot = document.getElementById("totalAmount")
+    elTot.value = total.toFixed(2);
+}
+
+function cancelorder () {
+    var isCancel = confirm("Are you sure to cancel order?")
+    if(isCancel){
+        window.location.reload();
+    } else {
+
+    }
+}
+
+
+window.onload = function() {
+    var elDt = document.getElementById("date");
+    var fmt = dtFormat(new Date());
+    elDt.value = fmt[1];
+
+    var elCust = document.getElementById("customerName");
+    elCust.focus();
+}
+
+
+var form = document.getElementById("book");
+form.onsubmit = function(e){
+    this.checkValidity();
+
+    e.preventDefault();
+    var total = totalAmount(3)
+    if(total > 0 ){
+        var isSubmit = confirm("Are you sure you wd like to submit book order?")
+        if(isSubmit) {
+            // submit data;
+            form.submit();
+            var custName = document.getElementById("customerName")
+            alert(custName.value + " your order has been successfully submitted. You will receive order in 10 day.");
+            window.print();
+        } else {
+            return false;
+        }
+    } else {
+        alert("Invalid total amount");
+        return false;
+    }
 }
