58ba0419d1fe594a144105079d0684a47fd29594
kahshiu <kahshiu@gmail.com>
2022-03-26 11:24:03 +0800

    checkpoint middle

--- batch-1000/Practical 6/Data File/Q1/general.js
+++ batch-1000/Practical 6/Data File/Q1/general.js
@@ -1,1 +1,52 @@
-﻿
+﻿function calcAmount(priceString,quantityString){
+    var priceNum = Number(priceString);
+    var quantityNum = Number(quantityString);
+    return priceNum * quantityNum;
+}
+
+//NOTE: triggered by bookname select dropdown
+function display(el, rowno){
+    var selectedIndex = el.selectedIndex;
+    var selectedValue = el.value;
+
+    var rowString = rowno.toString();
+    var elPrice = document.getElementById("Price" + rowString);
+    var elQuantity = document.getElementById("Quantity" + rowString);
+    var elAmount = document.getElementById("Amount" + rowString);
+
+    if(selectedIndex > 0){
+        elPrice.value = selectedValue;
+        elQuantity.disabled = false;
+
+        var amt = calcAmount(elPrice.value, elQuantity.value);
+        elAmount.value = amt.toFixed(2);
+    } else {
+        elPrice.value = "";
+        elQuantity.disabled = true;
+        elAmount.value = "0";
+    }
+}
+
+
+//NOTE: triggered by quantity select dropdown
+function sub(el, rowno){
+    var selectedIndex = el.selectedIndex;
+    var selectedValue = el.value;
+
+    var rowString = rowno.toString();
+    var elPrice = document.getElementById("Price" + rowString);
+    var elQuantity = document.getElementById("Quantity" + rowString);
+    var elAmount = document.getElementById("Amount" + rowString);
+
+    if(selectedIndex > 0){
+        //elPrice.value = selectedValue;
+        //elQuantity.disabled = false;
+
+        var amt = calcAmount(elPrice.value, elQuantity.value);
+        elAmount.value = amt.toFixed(2);
+    } else {
+        //elPrice.value = "";
+        //elQuantity.disabled = true;
+        elAmount.value = "0";
+    }
+}
